173e0742c66577e5502cc8ecdc378cb419df02a7
J. Sterling Bishop <jsterlingbishop@gmail.com>
2020-12-09 17:13:21 -0800

    Tests Work

--- test_app.py
+++ test_app.py
@@ -1,19 +1,43 @@
 from unittest import TestCase
 from app import app
+from models import db, User
 
+# Use test database and don't clutter tests with SQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
+app.config['SQLALCHEMY_ECHO'] = False
 
+# Make Flask errors be real errors, rather than HTML pages with error info
 app.config['TESTING'] = True
 
+# This is a bit of hack, but don't use Flask DebugToolbar
 app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
+db.drop_all()
+db.create_all()
 
 class BloglyTestCase(TestCase):
+
+    def setUp(self):
+        """Add test user."""
+
+        User.query.delete()
+
+        test_user = User(first_name="Tony", last_name="Hawk")
+        db.session.add(test_user)
+        db.session.commit()
+
+        self.test_id = test_user.id
+
+    def tearDown(self):
+        """Clean up any fouled transaction."""
+
+        db.session.rollback()
 
     def test_root_redirect(self):
         """ Tests that root routes are redirected """
         with app.test_client() as client:
             resp = client.get("/")
-   
+
             self.assertEqual(resp.status_code, 302)
             self.assertEqual(resp.location, "http://localhost/users")
 
@@ -26,7 +50,6 @@ class BloglyTestCase(TestCase):
 
             self.assertEqual(resp.status_code, 200)
             self.assertIn("Users:", html)
-
 
     def test_users_page(self):
         """ Checks that users page contains list of users """
@@ -59,12 +82,10 @@ class BloglyTestCase(TestCase):
             self.assertIn("Example", html)
             self.assertIn("<li", html)
 
-
-    # def test_(self):
-    #     """ """
-    #     with app.test_client() as client:
-
-    # def test_(self):
-    #     """ """
-    #     with app.test_client() as client:
-    
+    def test_user_details(self):
+        """ Test that user can be correctly created
+            and displayed in the /users route"""
+        with app.test_client() as client:
+            response = client.get(f'/users/{self.test_id }')
+            html = response.get_data(as_text=True)
+            self.assertIn("Tony Hawk", html)
