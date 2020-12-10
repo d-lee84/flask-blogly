7eac368c85e1ef1d6c212af84114adf0475ca52c
J. Sterling Bishop <jsterlingbishop@gmail.com>
2020-12-09 16:03:33 -0800

    Added routes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 """Blogly application."""
 
+from flask_debugtoolbar import DebugToolbarExtension
 from flask import Flask, request, redirect, render_template
 from models import db, connect_db, User
 
@@ -10,9 +11,87 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 
-from flask_debugtoolbar import DebugToolbarExtension
 app.config['SECRET_KEY'] = "SECRET!"
 debug = DebugToolbarExtension(app)
 
 
 db.create_all()
+
+
+@app.route("/")
+def redirect_to_users_list():
+    """ For now, redirects to a list of users. """
+    return redirect("/users")
+
+
+@app.route("/users")
+def show_users_list():
+    """ Shows all users with a link to the user's detail page. """
+    return render_template("user_listing.html", users=User.query.all())
+
+
+@app.route("/users/new")
+def show_create_user_page():
+    """ Shows create user form page. """
+    return render_template("new_user.html")
+
+
+@app.route("/users/new", methods=["POST"])
+def save_new_user_then_redirect():
+    """ Handles create new user form submission,
+    saves that new user to the database,
+    then redirects to /users. """
+    first_name, last_name, image_url = get_form_data()
+    # save to database
+    new_user = User(
+        first_name=first_name,
+        last_name=last_name,
+        image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
+    # redirect to all users list
+    return redirect("/users")
+
+
+@app.route("/users/<int:user_id>")
+def show_user_details(user_id):
+    """ Shows information about a user. """
+    user = User.query.get(user_id)
+    return render_template("user_detail.html", user=user)
+
+
+@app.route("/users/<int:user_id>/edit")
+def show_user_edit_page(user_id):
+    """ Shows the user edit page for a given User id. """
+    user = User.query.get(user_id)
+    return render_template("user_edit.html", user=user)
+
+
+@app.route("/users/<int:user_id>/edit", methods=["POST"])
+def save_user_edits_then_redirect(user_id):
+    """ Saves user edits into the database,
+    then redirects to users list. """
+    user = User.query.get(user_id)
+    user.first_name, user.last_name, user.image_url = get_form_data()
+    db.session.commit()
+    return redirect("/users")
+
+
+@app.route("/users/<int:user_id>/delete", methods=["POST"])
+def delete_user_then_redirect(user_id):
+    """ Deletes the user for the given id,
+    then redirects to users list. """
+    user = User.query.get(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect("/users")
+
+
+def get_form_data():
+    """ Grab form data and return as a list. """
+    profile_img = request.form.get('img_url')
+    profile_img = profile_img if profile_img else None
+
+    return [request.form.get('first_name'),
+            request.form.get('last_name'),
+            profile_img]
