8eb1b8da1b3078cd5e845ca4c11e74f519a9a250
J. Sterling Bishop <jsterlingbishop@gmail.com>
2020-12-09 17:37:23 -0800

    bug fixes

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 connect_db(app)
 
 app.config['SECRET_KEY'] = "SECRET!"
